8954744afa17f8b5f79770cbc3013dcbc0938073
88101999 <hayatosuzuki1221@gmail.com>
2026-01-29 23:40:24 +0900

    質問画面の実装>>選択肢もプルダウン形式で表示

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,26 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+questions_data = [
+  {
+    text: "あなたの性別を教えてください",
+    options: ["男性", "女性"]
+  },
+  {
+    text: "どんなシーンで着ますか？",
+    options: ["休日", "仕事"]
+  },
+  {
+    text: "どんなジャンルが好きですか？",
+    options: ["カジュアル系", "キレイ目系"]
+  }
+]
+
+questions_data.each do |data|
+  question = Question.new(text: data[:text]) do |q|
+    data[:options].each do |option_name|
+      q.options.build(name: option_name)
+    end
+  end
+  question.save
+end
