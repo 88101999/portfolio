0b5262818d06dd8a80a0619cea06a67190cd7f94
88101999 <hayatosuzuki1221@gmail.com>
2026-02-01 19:32:54 +0900

    コーディネート検索のロジックを実装

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,12 +1,13 @@
 class AnswersController < ApplicationController
   def create
+    answer_log = AnswerLog.create!(user_id: current_user.id)
     params.select { |k, _| k.to_s.start_with?("question_") }.each do |key, option_id|
       question_id = key.split("_").last.to_i
         Answer.create!(
           user_id: current_user.id,
           question_id: question_id,
           option_id: option_id,
-          answer_log_id: nil
+          answer_log_id: answer_log.id
         )
     end
     redirect_to coordinates_path
