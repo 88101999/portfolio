76772a29627ff2395c37cc5d73912c8d5429dd19
88101999 <hayatosuzuki1221@gmail.com>
2026-02-22 21:21:27 +0900

    seedファイルに質問とコーディネートデータを追加

--- app/models/coordinate_option.rb
+++ app/models/coordinate_option.rb
@@ -1,7 +1,7 @@
 class CoordinateOption < ApplicationRecord
-  belongs_to :coordinate
+  belongs_to :coordinate, inverse_of: :coordinate_options
   belongs_to :option
 
-  validates :coordinate_id, presence: true
+  validates :coordinate_id, presence: true, on: :update
   validates :option_id, presence: true, uniqueness: { scope: :coordinate_id }
 end
