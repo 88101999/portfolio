e4af229601d555af958a7a489d2278c18f491ea5
88101999 <hayatosuzuki1221@gmail.com>
2026-04-24 21:19:56 +0900

    お気に入り登録/解除時のフラッシュメッセージを実装

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,14 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import * as bootstrap from "bootstrap"
 window.bootstrap = bootstrap
+
+document.addEventListener('turbo:load', () => {
+  const flashMessages = document.querySelectorAll('.alert');
+  flashMessages.forEach((message) => {
+    setTimeout(() => {
+      message.style.transition = 'opacity 0.5s';
+      message.style.opacity = '0';
+      setTimeout(() => message.remove(), 500);
+    }, 3000); // 3秒後に消える
+  });
+});
